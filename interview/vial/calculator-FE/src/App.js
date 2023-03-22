1094d73056b1f7e27b647ef3c1638074a46be4e6
Justin Mulroney <justin@theryna.com>
2023-03-22 13:14:03 -0400

    add various buttons

--- interview/vial/calculator-FE/src/App.js
+++ interview/vial/calculator-FE/src/App.js
@@ -1,18 +1,51 @@
+import Wrapper from "./components/Wrapper";
+import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
+import Button from "./components/Button";
+import React, { useState } from "react";
 
-import './App.css';
+const btnValues = [
+  ['M+', "M-", "MR", "MC",],
+  ["C", "√", "%", "/"],
+  ['HIS ', '^', '+-',''],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
 
-function App() {
+
+const App = () => {
+
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-
-      
-      
-          Hello test
-
-      </header>
-    </div>
+    <Wrapper>
+      <Screen value="0" />
+      <ButtonBox>
+      {
+          btnValues.flat().map((btn, i) => {
+            return (
+              <Button
+                key={i}
+                className={btn === "=" ? "equals" : ""}
+                value={btn}
+                onClick={() => {
+                
+                  console.log(`${btn} clicked!`);
+                }}
+              />
+            );
+          })
+        }
+      </ButtonBox>
+    </Wrapper>
   );
-}
+};
 
 export default App;
